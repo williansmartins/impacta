4fe70f02c869ac92217861a367badc23373a38e8
Willians Martins <professorwillians@gmail.com>
2018-02-28 10:45:00 -0300

    corrindo sugeiras

--- src/main/webapp/js/scripts.js
+++ src/main/webapp/js/scripts.js
@@ -1,43 +1,22 @@
 $(document).ready(buscarDespesas());
 
 function buscarDespesas() {
-	
-    $.ajax({
-        url:'/despesas/todasDespesas',
-        complete: function (response) {
-<<<<<<< HEAD
-         //   $('#output').html(response.responseText);
-            var despesas = response.responseJSON;
-            addItensNaTabela(despesas);
-=======
-            //$('#output').html(response.responseText);
-        	var despesas = response.responseJSON;
-        	addItensNaTabela(despesas);
->>>>>>> 03f92fb5ec5cfe80329f14e54798bb19fe550f46
-        },
-        error: function () {
-            $('#output').html('Ixi: there was an error!');
-        },
-    });
-    return false;
-<<<<<<< HEAD
+	$.ajax({
+		url:'/despesas/todasDespesas',
+		complete: function (response) {
+			var despesas = response.responseJSON;
+			addItensNaTabela(despesas);
+		},
+		error: function () {
+			$('#output').html('Ixi: there was an error!');
+		},
+	});
+}
     
-    function addItensNaTabela(despesas){
-    	    	
-    	for (var i = 0 ; i<despesas.length; i++ ){
-    		
-    	var despesa = despesas[i];
-    	$('tabela').append("<tr><td>+despesa.codigo+</td><td>+despesa.Categoria+</td><td>+despesa.data+</td></tr>");
-    }
-  }
-}
-=======
-}
-
 function addItensNaTabela(despesas){
 	for(var i = 0; i<despesas.length; i++){
 		var despesa = despesas[i];
-		$('#tabela').append("<tr><td>"+despesa.codigo+"</td><td>"+despesa.categoria+"</td><td>"+despesa.descricao+"</td><td>"+despesa.valor+"</td></tr>");
+		$('#tabela').append("<tr><td>"+despesa.codigo+"</td><td>"+despesa.categoria+"</td><td>"+despesa.descricao+"</td><td>"+despesa.valor+"</td><td>"+despesa.data+"</td><td>"+despesa.observacoes+"</td></tr>");
 	}
 }
 
@@ -48,5 +27,3 @@ function addItensNaTabela(despesas){
 
 
 
-
->>>>>>> 03f92fb5ec5cfe80329f14e54798bb19fe550f46
