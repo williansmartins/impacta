4a4ea14abd90116080b225bb12339ddddeb2bb02
Alexandre Lima <ale_xandre@ig.com.br>
2018-02-28 10:04:13 -0300

    add data

--- src/main/webapp/js/scripts.js
+++ src/main/webapp/js/scripts.js
@@ -2,11 +2,22 @@ function getSuccessOutput() {
     $.ajax({
         url:'/despesas/todasDespesas',
         complete: function (response) {
-            $('#output').html(response.responseText);
+         //   $('#output').html(response.responseText);
+            var despesas = response.responseJSON;
+            addItensNaTabela(despesas);
         },
         error: function () {
             $('#output').html('Ixi: there was an error!');
         },
     });
     return false;
+    
+    function addItensNaTabela(despesas){
+    	    	
+    	for (var i = 0 ; i<despesas.length; i++ ){
+    		
+    	var despesa = despesas[i];
+    	$('tabela').append("<tr><td>+despesa.codigo+</td><td>+despesa.Categoria+</td><td>+despesa.data+</td></tr>");
+    }
+  }
 }
