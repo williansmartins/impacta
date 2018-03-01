066a95c0a46e06d0b55a13f2906430cb1f5d999e
di4510@CLASSROOM.NET <di4510@CLASSROOM.NET>
2018-03-01 09:41:07 -0300

    AlunoRepositoryTests atualizado com os 4 tests

--- src/test/java/br/com/impacta/springmvc/gerenciadordespesas/AlunosRepositoryTests.java
+++ src/test/java/br/com/impacta/springmvc/gerenciadordespesas/AlunosRepositoryTests.java
@@ -24,7 +24,7 @@ public class AlunosRepositoryTests {
 		
 		List<Alunos> lista = dao.findAll();
 		System.out.println(lista);
-//		Assert.assertEquals(???, ???);
+
 	}
 
 	@Test
@@ -41,5 +41,39 @@ public class AlunosRepositoryTests {
 		dao.delete(aluno);
 		Alunos findOne = dao.findOne(cod);
 		Assert.assertNull(findOne);
+	}
+	
+	@Test
+	public void atualizar(){
+		Alunos alunoLocal = new Alunos();
+		alunoLocal.setNome("manuel");
+		alunoLocal.setCpf("386.513.458-01");
+		alunoLocal.setDatanasc(new Date());
+		dao.save(alunoLocal);
+		
+		Alunos alunoBanco = dao.findOne(alunoLocal.getCod());
+		alunoBanco.setNome("joão");
+		dao.save(alunoBanco);
+		
+		Assert.assertEquals(alunoLocal.getCod(), alunoBanco.getCod());
+		Assert.assertNotEquals(alunoLocal.getNome(), alunoBanco.getNome());
+		
+		
+	}
+	
+	@Test
+	public void excluir(){
+		
+		Alunos alunoLocal1 = new Alunos();
+		alunoLocal1.setNome("manuel");
+		alunoLocal1.setCpf("386.513.458-01");
+		alunoLocal1.setDatanasc(new Date());
+		dao.save(alunoLocal1);
+		Alunos alunoBanco1 = dao.findOne(alunoLocal1.getCod());
+		Assert.assertNotNull(alunoBanco1);
+		dao.delete(alunoBanco1);
+		Alunos alunoBanco2 = dao.findOne(alunoLocal1.getCod());
+		Assert.assertNull(alunoBanco2);
+		
 	}
 }
