886e406678db1bd235915fb9d81e7fea1929b0d6
Willians Martins <professorwillians@gmail.com>
2018-03-01 11:17:28 -0300

    add template

--- src/main/webapp/js/scripts.js
+++ src/main/webapp/js/scripts.js
@@ -5,7 +5,7 @@ function buscarDespesas() {
 		url:'/despesas/todasDespesas',
 		complete: function (response) {
 			var despesas = response.responseJSON;
-			addItensNaTabela(despesas);
+			addItensNaTela(despesas);
 		},
 		error: function () {
 			$('#output').html('Ixi: there was an error!');
@@ -17,6 +17,13 @@ function addItensNaTabela(despesas){
 	for(var i = 0; i<despesas.length; i++){
 		var despesa = despesas[i];
 		$('#tabela').append("<tr><td>"+despesa.codigo+"</td><td>"+despesa.categoria+"</td><td>"+despesa.descricao+"</td><td>"+despesa.valor+"</td><td>"+despesa.data+"</td><td>"+despesa.observacoes+"</td><td><a href='#' class='btn btn-success'>editar</a>&nbsp;<a href='#' class='btn btn-danger'>excluir</a></td></tr>");
+	}
+}
+
+function addItensNaTela(despesas){
+	for(var i = 0; i<despesas.length; i++){
+		var despesa = despesas[i];
+		$('#cards').append("<div class='col-lg-4 mb-4'> <div class='card h-100'> <h4 class='card-header'>"+despesa.descricao+"</h4> <div class='card-body'> <p class='card-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p> </div> <div class='card-footer'> <a href='#' class='btn btn-primary'>Learn More</a> </div> </div> </div>");
 	}
 }
 
